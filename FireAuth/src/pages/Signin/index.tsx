ed7e6b48dd3e5864d2bd84fb7742095bcbda6e37
Isaac Sousa <131274652+IsaacSousa2@users.noreply.github.com>
2024-06-29 16:53:04 -0300

    Finalizando projeto de autenticação com google

--- FireAuth/src/pages/Signin/index.tsx
+++ FireAuth/src/pages/Signin/index.tsx
@@ -1,11 +1,35 @@
 import '../Signin/styles.scss'
 import { AiOutlineGoogle } from "react-icons/ai";
+import { GoogleAuthProvider, User, signInWithPopup }  from 'firebase/auth'
+import { auth } from '../../services/firebase'
+import { useState } from 'react';
 
 export function SignIn(){
+
+    const [user, setUser] = useState<User>({} as User);
+
+    function handleGoogleSignIn() {
+
+        const provider = new GoogleAuthProvider()
+
+        signInWithPopup(auth, provider)
+        .then((result) => [setUser(result.user)]) 
+        .catch((error) => [console.log(error)]) 
+    }
 
     return(
 
         <div className="container">
+
+            <div className='user'>
+                {user.photoURL && <img className='image' src={user.photoURL} alt="Foto do usuário" />}
+            </div>
+            <div className='userdata'>
+                <strong className='nome'>{user.displayName}</strong>
+
+                <small className='email'>{user.email}</small>
+            </div>
+
             <h1 className="">
                Acesse sua conta
             </h1>
@@ -14,7 +38,7 @@ export function SignIn(){
                 Ultilizando autênticação social
             </span>
 
-            <button type="button" className="button">
+            <button type="button" className="button" onClick={handleGoogleSignIn}>
                 < AiOutlineGoogle className='glogo' size={28} />
                 Entrar com google
             </button>
