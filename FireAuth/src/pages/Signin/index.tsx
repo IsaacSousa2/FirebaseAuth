24b86d791a3902ef5eb2333be64c303e26f63592
Isaac Sousa <131274652+IsaacSousa2@users.noreply.github.com>
2024-06-29 14:18:31 -0300

    Adicionando ligação com API do firebase

--- FireAuth/src/pages/Signin/index.tsx
+++ FireAuth/src/pages/Signin/index.tsx
@@ -1,11 +1,23 @@
+import '../Signin/styles.scss'
+import { AiOutlineGoogle } from "react-icons/ai";
+
 export function SignIn(){
 
     return(
 
-        <div className="">
+        <div className="container">
             <h1 className="">
-                SignIn
+               Acesse sua conta
             </h1>
+
+            <span className="">
+                Ultilizando autênticação social
+            </span>
+
+            <button type="button" className="button">
+                < AiOutlineGoogle className='glogo' size={28} />
+                Entrar com google
+            </button>
         </div>
 
     )
---
+++ FireAuth/src/services/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3q_YIvWnFf8wqVKUkX6RRjMpf7w8_Tao",
+  authDomain: "auth-1da83.firebaseapp.com",
+  projectId: "auth-1da83",
+  storageBucket: "auth-1da83.appspot.com",
+  messagingSenderId: "102435302026",
+  appId: "1:102435302026:web:01a4d724a45d7d0eb75b73"
+};
+
+
+const app = initializeApp(firebaseConfig);
