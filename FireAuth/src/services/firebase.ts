851d605fe17f86c8b6c823eb2756fcd20dfcdc9c
Isaac Sousa <131274652+IsaacSousa2@users.noreply.github.com>
2024-06-29 16:09:01 -0300

    Corrigindo bugs

--- FireAuth/src/services/firebase.ts
+++ FireAuth/src/services/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
   apiKey: "AIzaSyB3q_YIvWnFf8wqVKUkX6RRjMpf7w8_Tao",
@@ -12,3 +12,4 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
